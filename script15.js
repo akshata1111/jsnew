d857ac8839c8e4fd7e5d490cf97ee5cafb15fa26
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-17 17:07:51 +0530

    added

---
+++ script15.js
@@ -0,0 +1,75 @@
+// let i1 = 1
+// while (i1 <= 3) {
+//     console.log("hello") 
+//     i1 = i1 + 1 
+// }
+
+// let i2 = 1
+// while (i2 <= 5) {
+//     console.log(i2)
+//     i2 = i2 + 1
+// }
+
+// let i3 = 5
+// while (i3 >= 1) {
+//     console.log(i3) 
+//     i3 = i3 - 1 
+// }
+
+// let i4 = 2
+// while (i4 <= 20) {
+//     console.log(i4)
+//     i4 = i4 + 2
+// }
+
+// let i5 = 50
+// while (i5 >= 5) {
+//     console.log(i5)
+//     i5 = i5 - 5
+// }
+
+// let i6 = 30
+// while (i6 >= 3) {
+// console.log(i6)
+//     i6 = i6 - 3
+// }
+
+let i7 = 1
+while (i7 <= 5) {
+ console.log(i7) 
+    if (i7 == 2) {
+        break
+    }
+    i7 = i7 + 1 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
