be197289cb213a7cd2ded0fddeeb592fd5a0dec0
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-17 16:56:22 +0530

    added

--- script13.js
+++ script13.js
@@ -1,9 +1,9 @@
-let city  = "pune"
+// let city  = "pune"
 
-// switch(city){
+//    switch(city){
 
 //     case "pune":
-//         console.log("MH")
+//         console.log("KT")
 //     case "jaipur":
 //         console.log("RJ")
 //     case "banglore":
@@ -12,104 +12,60 @@ let city  = "pune"
 //         console.log('Incorrect cityName')
 // }
 
+// let city = "Pune"
 // switch(city){
-//     case "pune":
+//     case "Pune":
 //         console.log("MH")
 //         break
-//     case "jaipur":
+//     case "Jaipur":
 //         console.log("RJ")
 //         break
-//     case "banglore":
-//         console.log("KA")
+//     case "Bhopal":
+//         console.log("MP")
 //         break
 //     default:
-//         console.log('Incorrect cityName')
+//         console.log("Incorrect city")
 // }
 
+// let city = "Indore"
 // switch(city){
-//     case "pune":
-//     case "nagpur":
+//     case "Pune":
+//     case "Nagpur":
 //         console.log("MH")
 //         break
-//     case "jaipur":
-//     case "udaipur":
+//     case "Jaipur":
+//     case "Udaipur":
 //         console.log("RJ")
 //         break
-//     case "banglore":
-//     case "otty":
-//         console.log("KA")
+//     case "Bhopal":
+//     case "Indore":
+//         console.log("MP")
 //         break
 //     default:
-//         console.log('Incorrect cityName')
+//         console.log("Incorrect city")
 // }
 
-// let a = 15
-// let b = 5
-// let c = 2
-
+// let x1 = 10
+// let x2 = 5
+// let x3 = 2
 // let isGreatest = true
 // switch(isGreatest){
-//     case a > b && a > c:
-//         console.log("a is greater")
+
+//     case x1 > x2 && x1 > x3:
+//         console.log("x1 is greater")
 //         break
-//     case b > a && b > c:
-//         console.log("b is greater")
-//         break
+//     case x2 > x1 && x2 > x3:
+//         console.log("x2 is greater")
+//         break 
 //     default:
-//         console.log("c is greater")
+//         console.log("x3 is greater")
+
 // }
 
-
-// loops 
-// for loop and while loop 
-// for(intialization ; conditionCheck ;increment/decrement){
-//     // statements
+// for (let i = 1; i <= 3; i++) 
+//     console.log(i)  
 // }
 
-
-// program 1
-
-for(let i = 1 ; i <= 3 ; i++){ // 2 // 3 // 4
-    console.log(i) // 1 // 2 // 3
-}
-
-//1 <= 3 ? true
-//2 <= 3 ? true
-//3 <= 3 ? true
-
-
-// program 2
-// print 1 to 5
-for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
-    console.log(i) // 1 // 2 // 3 // 4 // 5
-}
-
-// program 3
-// print 5 to 1
-for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
-    console.log(i) // 5 // 4 // 3 // 2 //1
-}
-
-// program 4
-// table of 2 
-for(let i = 2 ; i <= 20 ; i = i + 2){ // 4 ------------ 22
-    console.log(i) // 2 ---------------- 20
-}
-
-// program 5
-// table of 2
-for(let i = 1 ; i <= 10 ; i++){
-    console.log(i * 2)
-}
-
-// program 6 
-// table of 5 in reverse 
-for(let i = 50 ; i >= 5 ; i = i - 5){
-    console.log(i)
-}
-
-// program 3 
-// table of 3 in reverse 
-for(let i = 30 ; i >= 3 ; i = i - 3){
-    console.log(i)
+for (let i = 1; i <= 5; i++) { 
+    console.log(i) 
 }
---
+++ script14.js
@@ -0,0 +1,34 @@
+// for(let i = 1 ; i <= 3 ; i++){ 
+//     console.log(i) 
+// }
+
+// for(let i = 1 ; i <= 5 ; i++){
+//     console.log(i)
+// }
+
+// let c=3
+// for(let i = 1 ; i <= c ; i++){
+//     console.log("hello")
+//     console.log("bye")
+// }
+
+// for(let i = 1 ; i <= 5 ; i++){ 
+//     if(i == 3){
+//         break
+//     }
+//     console.log(i) 
+// }
+
+// for(let i = 1 ; i <= 5 ; i++){  
+// console.log(i)  
+//     if(i == 3){
+//         break
+//     }
+// }
+
+for(let i = 5 ; i >= 1 ; i--){ 
+    if(i == 3){
+        break
+    }
+    console.log(i)
+}
