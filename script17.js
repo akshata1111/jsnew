740656ddd9423403f5ae70c0bcb1605c0720a546
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-19 19:25:53 +0530

    added

---
+++ script17.js
@@ -0,0 +1,41 @@
+// let names = ["minal","purva",'shradha',"sachin","ram"]
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+
+// let q1 = names.length
+// console.log(q1)
+
+// for(let i = 0 ; i < 5 ; i++){
+//     console.log(i)
+// } 
+
+
+// let animal = ["tiger","lion","cat","dog"]
+// //console.log(animal[0])
+// console.log(animal.length)
+// for(let i = 0 ; i<animal.length ; i++){
+//     console.log(i)
+//     //console.log(animal[i])
+// }
+
+// let flowers = ["rose","lily","jasmine","sunflower"]
+// for(let i = 0 ; i < flowers.length ; i++){
+//     // console.log(i)
+//     // console.log(flowers[i])
+
+//     if(flowers[i] == "jasmine"){
+//         continue
+//     }
+//     console.log(flowers[i])
+// }
+
+let country =  ["india","srilanka","bangladesh","nepal"]
+console.log(country)
+let y = 0
+while(y < country.length){
+    console.log(y)
+    console.log(country[y])
+    y = y + 1
+
+}
