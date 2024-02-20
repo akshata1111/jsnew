1affee8ded5c897cbaabce72e3aae8feab012683
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-20 13:18:03 +0530

    added

---
+++ script19.js
@@ -0,0 +1,37 @@
+// let birthyear = [ 1989,1990, 1991, 1992]
+// for(let i = 0; i < 4; i++){
+
+// // console.log(i)
+// // console.log(birthyear[i])
+// console.log(2024 - birthyear[i])
+// }
+
+// let marks = [11,44,66,8,33,44,55]
+
+// for(let i = 0; i < marks.length; i++){
+//     // console.log(i)
+// // console.log(marks[i])
+
+// if (marks[i] > 50){
+//     console.log(marks[i])
+// }
+// }
+
+// let numbers = [11,22,33]
+// let total = 0
+// for(let i = 0; i < numbers.length; i++){
+//     // console.log(i)
+//     // console.log(numbers[i])
+//     total = total + numbers[i]
+
+// }
+//  console.log(total)
+
+let cities = ["nagpur", "chennai", "banglore", "kolkata"]
+for(let i = 0; i < cities.length; i++){
+    console.log(i)
+    console.log("welcome" + cities[i])
+}
+
+
+
