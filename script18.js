9feab0264032add4fa56fafe40d120a0bef0ca8e
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-20 13:00:48 +0530

    added

---
+++ script18.js
@@ -0,0 +1,75 @@
+// let names = ["chinmay", "sham", "ram", "satish"]
+// let q1 = names.push("sarika")
+// console.log(q1)
+// console.log(names)
+
+// let q2 = names.unshift("purva")
+// console.log(q2)
+// console.log(names)
+
+// let fruits = ["apple", "mango", "banana", "grapes"]
+// // let q3 = fruits.pop()
+// // console.log(q3)
+// // console.log(fruits)
+
+// let q4 = fruits.shift()
+// console.log(q4)
+// console.log(fruits)
+
+// let animals = ["tiger", "lion", "bear", "cat"]
+// // let q5 = animals.indexOf('lion')
+// // console.log(q5)
+// let q6 = animals.indexOf('Lion')
+// console.log(q6)
+
+// let city = ["Bng", "kolkata", "chennai", "ngp"]
+// let q7 = city.includes('chennai')
+// console.log(q7)
+// let q8 = city.includes('hanoi')
+// console.log(q8)
+
+// let q9 = city.reverse()
+// console.log(city)
+// console.log(q9)
+
+// let country = ["india", "pakistan", "bangladesh", "srilanka"]
+// let q10 = country.at(3)
+// console.log(q10)
+
+// let a = [ 11, 22, 33]
+// let b = [44, 55, 66]
+// let q11 = a.concat(b)
+// console.log(q11)
+// console.log(a)
+// console.log(b)
+
+let info = ["chinmay", "deshpande", 77777]
+let q12 = info.join('-')
+console.log(q12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
