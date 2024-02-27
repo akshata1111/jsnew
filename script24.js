04d9b8ab5bf28b6b9e5b2db37fbf39ff0ead28da
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-27 15:15:10 +0530

    added

---
+++ script24.js
@@ -0,0 +1,24 @@
+// let names=["Mayuri","Vaibhav","Ram","sham"]
+// names.push("sita")
+// console.log(names)
+
+// console.log(names[1])
+
+// names[2]="rajesh"
+// console.log(names)
+
+
+// names.pop()
+// console.log(names)
+
+
+
+let info=["Mayuri","Kumar",23,36]
+let info = {
+    firstName: "Mayuri",
+    lastName: "Kumar",
+    rollNumber: 26,
+    age: 36
+}
+console.log(info.lastName)
+console.log(info.age)
