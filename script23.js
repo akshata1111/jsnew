620ca52aea22d135371a65f11a7e621ebda06ab0
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-27 12:30:50 +0530

    added

---
+++ script23.js
@@ -0,0 +1,47 @@
+// let cities = ["pune", "mumbai","bng", "kolkata", "chennai"]
+// cities.pop()
+// cities.shift()
+// cities.splice(1,2)
+// console.log(cities)
+
+
+// let numA = [11,22,33,44,55,66]
+// let q = numA.fill('@',1,4)
+// console.log(q)
+
+
+// let states = [["ngp","wardha"],["bhopal","indore"],["Jaipur","udaipur"]]
+// console.log(states[0][1])
+// console.log(states[1][1])
+// console.log(states[2][1])
+// // let a11 = states.flat()
+// // console.log(states)
+
+
+// let fruits = ["apple", "banana","grapes", "chiku"]
+// fruits.sort()
+// console.log(fruits)
+
+
+let a12 = [11,22,33,44,55,66]
+let b11 = a12.indexOf(33)
+let b12 = a12.flat(3)
+console.log(b11)
+console.log(b12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
