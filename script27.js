1a7c3610e2eeefcb7d57c3629a96e98acb7b3826
akshata1111 <akshata.paltiwale.ap@gmail.com>
2024-02-28 18:49:28 +0530

    added

---
+++ script27.js
@@ -0,0 +1,48 @@
+// let info = ["chinmay","deshpande", 34, 67]
+
+// let chinmay = {
+ 
+    
+//     firstName: "chinmay",
+//     lastName: "deshpande",
+//     age: 34,
+//     rollNo: 67
+// }
+
+// console.log(chinmay)
+
+// console.log(chinmay.firstName)
+// console.log(chinmay['firstName'])
+
+// chinmay.lastName = "dani"
+// chinmay['lastName'] = "deshmukh"
+// console.log(chinmay)
+
+
+
+// chinmay['language'] = "marathi"
+// chinmay.city = "pune"
+// console.log(chinmay)
+
+
+
+// delete chinmay.firstName
+// delete chinmay['lastName']
+
+
+vehicle = {
+    color: "red",
+    type: "sedan"
+}
+
+
+console.log(vehicle.color)
+vehicle.color = "blue"
+vehicle.regNo = 123
+delete vehicle.color
+
+
+// console.log(vehicle['color'])
+// vehicle['color'] = "blue"
+// vehicle['regNo'] = 123
+// delete vehicle['regNo']
